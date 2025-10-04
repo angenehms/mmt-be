a82d89f10f3dd2bfb548bcec1fe8e47c8262e5a2
angenehms <angenehms@gmail.com>
2025-10-04 13:35:51 +0900

    [BE] Chore : Group endpoints (MBA-17)

--- src/main/java/common/config/SwaggerConfig.java
+++ src/main/java/common/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package common.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,6 +27,23 @@ public class SwaggerConfig {
                                 .url("http://localhost:8080")
                                 .description("개발용 서버")
                 ));
+    }
+
+    // 엔드포인트 버젼별 대그룹화 (스웨거 페이지 우측 상단 위치)
+    @Bean
+    public GroupedOpenApi groupedOpenApiV1() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/api/v1/**") // 해당 경로로 작성된 엔드포인트들을 v1 그룹에 그룹화
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApiV2() {
+        return GroupedOpenApi.builder()
+                .group("v2")
+                .pathsToMatch("/api/v2/**")
+                .build();
     }
 
 }
