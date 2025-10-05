0915bf51bc2c42298613703e9c5b8c471f423dee
angenehms <angenehms@gmail.com>
2025-10-05 17:23:22 +0900

    [BE] Feat : Add securityFilterChain setting (MBA-24)

--- src/main/java/ssafy/mmt/common/config/JpaAuditingConfig.java
+++ src/main/java/ssafy/mmt/common/config/JpaAuditingConfig.java
@@ -3,6 +3,7 @@ package ssafy.mmt.common.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// 엔터티 createdAt, modifiedAt 자동화
 @Configuration
 @EnableJpaAuditing // MmtApplication 메인함수에 어노테이션 달아도 되는데 통합테스트에 문제가 될 수 있어 따로 config 파일 만들어 관리
 public class JpaAuditingConfig {
--- src/main/java/ssafy/mmt/common/config/SecurityConfig.java
+++ src/main/java/ssafy/mmt/common/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package ssafy.mmt.common.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -15,4 +20,48 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    // SecurityFilterChain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // CSRF 보안 필터 disable (커스텀 세팅) - stateless 한 서버에서 필요 없기에 꺼주고
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        // CORS 설정 (커스텀 세팅) - 리액트, 스프링과 같이 프론트, 백에 서로 다른 오리진을 가지는 경우에는 CORS 설정이 필수
+
+        // 기본 Form 기반 인증 필터들 disable (커스텀 세팅) - multipart form 데이터 형태로 받지 않고 우리는 json body 기반으로 받을 거기 때문에
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        // 기본 Basic 인증 필터 disable (커스텀 세팅) - httpBasic 기반의 로그인도 하지 않을 거기 때문에 꺼주고
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        // 인가 (커스텀 세팅) - 컨트롤러 api 에 대해 접근을 허용할건지 말건지 결정
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+
+        // 예외 처리 (커스텀 세팅)
+        http
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 401 응답 // 로그인을 하지 않은 상태로 접근한 경우
+                        })
+                        .accessDeniedHandler((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_FORBIDDEN); // 403 응답 // 권한이 없는 경우
+                        })
+                );
+
+        // 세션 필터 설정 (STATELESS) (커스텀 세팅) - stateless 한 설정 세팅
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+
 }
