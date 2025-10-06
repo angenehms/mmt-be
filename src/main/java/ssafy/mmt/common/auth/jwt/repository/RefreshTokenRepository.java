c6e3922c22be286993f8b6ba2b285f1e0ddc5dbd
angenehms <angenehms@gmail.com>
2025-10-07 08:19:58 +0900

    [BE] Feat : Add addRefreshToken, isRefreshTokenExist, removeRefreshToken, removeRefreshTokenMemeber in JWTService (MBA-30)

--- src/main/java/ssafy/mmt/common/auth/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/ssafy/mmt/common/auth/jwt/repository/RefreshTokenRepository.java
@@ -1,7 +1,16 @@
 package ssafy.mmt.common.auth.jwt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import ssafy.mmt.common.auth.jwt.entitiy.RefreshToken;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Boolean existsByRefreshToken(String refreshToken);
+
+    @Transactional // delete 동작의 경우 transactional 을 열어줘야함 -> 수정이나 삭제 쿼리 수행시 필요
+    void deleteByRefreshToken(String refreshToken);
+
+    @Transactional
+    void deleteByUsername(String username);
 }
