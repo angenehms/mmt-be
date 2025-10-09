bbfdbc96a044e8f6f60f040a412ee4841885160b
angenehms <angenehms@gmail.com>
2025-10-09 17:11:35 +0900

    [BE] Feat : Member CRUD (MBA-34)

--- src/main/java/ssafy/mmt/common/auth/jwt/application/JWTService.java
+++ src/main/java/ssafy/mmt/common/auth/jwt/application/JWTService.java
@@ -44,7 +44,7 @@ public class JWTService {
 
     // 특정 유저 Refresh 토큰 모두 삭제 (탈퇴)
     @Transactional
-    public void removeRefreshTokenMember(String refreshToken) {
-        refreshTokenRepository.deleteByUsername(refreshToken);
+    public void removeRefreshTokenMember(String username) {
+        refreshTokenRepository.deleteByUsername(username);
     }
 }
--- src/main/java/ssafy/mmt/domain/member/presentation/MemberController.java
+++ src/main/java/ssafy/mmt/domain/member/presentation/MemberController.java
@@ -4,13 +4,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ssafy.mmt.domain.member.application.MemberService;
 import ssafy.mmt.domain.member.dto.request.MemberVaildRequest;
+import ssafy.mmt.domain.member.dto.response.MemberInfoResponse;
 
+import java.nio.file.AccessDeniedException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -46,8 +45,28 @@ public class MemberController {
     }
 
     // 유저 정보
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public MemberInfoResponse userMeApi() {
+        return memberService.readMember();
+    }
 
     // 유저 수정 (자체 로그인 유저만)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> updateUserApi(
+            @Validated(MemberVaildRequest.updateGroup.class)
+            @RequestBody MemberVaildRequest mvr
+    ) throws AccessDeniedException {
+        return ResponseEntity.status(200).body(memberService.updateMember(mvr));
+    }
 
     // 유저 제거 (자체/소셜)
+    @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Boolean> deleteUserApi(
+            @Validated(MemberVaildRequest.deleteGroup.class)
+            @RequestBody MemberVaildRequest mvr
+    ) throws AccessDeniedException {
+
+        memberService.updateMember(mvr);
+        return ResponseEntity.status(200).body(true);
+    }
 }
