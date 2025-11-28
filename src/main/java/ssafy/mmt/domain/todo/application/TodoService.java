4f0d925147352c1de4085cf232559a23afbc0261
angenehms <angenehms@gmail.com>
2025-11-28 15:55:29 +0900

    [BE] Feat : Add addTodo feature without exception

--- src/main/java/ssafy/mmt/domain/todo/application/TodoService.java
+++ src/main/java/ssafy/mmt/domain/todo/application/TodoService.java
@@ -2,6 +2,10 @@ package ssafy.mmt.domain.todo.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ssafy.mmt.common.auth.CustomMemberPrincipal;
+import ssafy.mmt.domain.todo.dto.request.TodoCreateRequest;
+import ssafy.mmt.domain.todo.entity.Todo;
 import ssafy.mmt.domain.todo.repository.TodoRepository;
 
 @Service
@@ -9,6 +13,20 @@ import ssafy.mmt.domain.todo.repository.TodoRepository;
 public class TodoService {
 
     private final TodoRepository todoRepository;
+
+    @Transactional
+    public void addTodo (
+            TodoCreateRequest tcr,
+            CustomMemberPrincipal customMemberPrincipal)
+    {
+        Long memberId = customMemberPrincipal.getMemberId();
+        Todo newTodo = Todo.builder()
+                .memberId(memberId)
+                .isDone(false)
+                .content(tcr.content())
+                .build();
+        todoRepository.save(newTodo);
+    }
 
 
 }
