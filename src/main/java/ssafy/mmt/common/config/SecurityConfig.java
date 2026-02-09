a6053c58952f83e4c77890e825c9d6c64cf27d68
angenehms <angenehms@gmail.com>
2026-02-10 04:50:16 +0900

    [BE] Chore : CORS 설정

--- src/main/java/ssafy/mmt/common/config/SecurityConfig.java
+++ src/main/java/ssafy/mmt/common/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // 허용 포트
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://mmtproject.store")); // 허용 포트
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용 메서드
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
